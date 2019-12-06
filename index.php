1d35c41fa97487b7f6f59a080f2a08e5460c7b3f
429273-11 <429273-11>
2019-12-06 17:37:53 +0300

    fourth commit

--- index.php
+++ index.php
@@ -7,4 +7,22 @@ include_once("Korusha/KorushaException.php");
 include_once("Korusha/Linear.php");
 include_once("Korusha/Log.php");
 include_once("Korusha/Square.php");
+$co_arr = [];
+foreach(["a", "b", "c"] as $co) {
+	$co_arr[$co] = readline("Enter ".$co.": ")?:0;
+}
+$a = $co_arr["a"];
+$b = $co_arr["b"];
+$c = $co_arr["c"];
+//Korusha\Log::log("Entered numbers: " . implode(", ", $co_arr));
+Korusha\Log::log("Equation: $a*x^2 + $b*x + $c = 0");
+try {
+	$solver = new Korusha\Square($a, $b, $c);
+	
+	Korusha\Log::log("Roots: " . implode(", ", $solver->solve($a, $b, $c)));
+	
+}catch(Korusha\KorushaException $ex) {
+	Korusha\Log::log($ex->getMessage());
+} 
+Korusha\Log::write();
 ?>
